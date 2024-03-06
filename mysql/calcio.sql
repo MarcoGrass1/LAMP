88180ef763cb769dc5c8377f366aadc82e9d00d0
MarcoGrass1 <m4rcograss145@gmail.com>
2024-03-06 08:04:13 +0000

    update

--- mysql/calcio.sql
+++ mysql/calcio.sql
@@ -1,11 +1,15 @@
-CREATE DATABASE IF NOT EXIST Calcio;
+CREATE DATABASE IF NOT EXISTS Calcio;
+USE Calcio;
+SHOW DATABASES;
 
-create table if not EXIST squdre(
+CREATE TABLE IF NOT EXISTS squadre(
     ID_Squadre int auto_increment primary key not null,
     nome varchar(30)
-)
+);
+SHOW TABLES;
 
-create table if not exist calciatori(
+
+create table if not exists calciatori(
     ID_calciatori int primary key auto_increment not null,
     Cognome varchar(30) not null,
     ruolo int, 
@@ -13,12 +17,19 @@ create table if not exist calciatori(
     nascita date not null,
     capitano int,
     fk_squadra int,
-    foreing key fk_squadra references squdre(ID_Squadre) on update cascade on delete no action
-)
-create table if not exist valutazioni(
+    foreign key (fk_squadra) references squadre(ID_Squadre) on update cascade on delete no action
+);
+SHOW TABLES;
+
+create table if not exists valutazioni(
     ID_valutazioni int primary key auto_increment not null,
     voto int not null,
     dsata_partita date not null,
     fk_calciatore int,
-    foreing key fk_calciatore references calciatori(ID_calciatori) on update cascade on delete no action
-)
+    foreign key (fk_calciatore) references calciatori(ID_calciatori) on update cascade on delete no action
+);
+SHOW TABLES;
+
+alter table calciatori CHANGE ruolo ruolo varchar(30);
+insert into squadre (ID_Squadre,nome) values(1,'Milan');
+insert into calciatori values(1,'Grassi','trequartista',21000,'2005/05/16',3,1);
