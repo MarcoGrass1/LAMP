85789d066e8447a39af7cccf367b720a42dd5936
MarcoGrass1 <m4rcograss145@gmail.com>
2024-03-14 10:47:20 +0000

    update

---
+++ www/es_b/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+<style> a{border: solid black 1px; background-color:#e6e6e6; border-radius:5px;}</style>
+<p><a href="login.php">Log In</a></p>
+<?php
+    session_start();
+    echo '<p><a href="riservata.php">Pagina Riservata</a></p>';   
+ ?>   
+</body>
+</html>
